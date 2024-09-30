5ae46f553948c0be8f4275e94d8687f4196e0983
William Howell <9555113+willowell@users.noreply.github.com>
2024-09-30 01:35:36 -0400

    InfiniteLists comment updates, remove extraneous lazy attr

--- advent-of-code-scala/src/core/data/InfiniteLists.scala
+++ advent-of-code-scala/src/core/data/InfiniteLists.scala
@@ -2,8 +2,14 @@ package core.data.infinitelists
 
 import scala.collection.immutable.LazyList
 
-lazy val naturals: LazyList[Int] = LazyList.from(1)
+/** Lazy list of natural numbers starting with 1. */
+val naturals: LazyList[Int] = LazyList.from(1)
 
+/** Iterator of natural numbers starting with 1. */
 val naturalsIt: Iterator[Int] = Iterator.from(1)
 
+// NB: `fibs` below is `lazy` because its definition is recursive.
+// `naturals` and `naturalsIt` do not refer to themselves in their definitions, so this is not needed there.
+
+/** Lazy list of fibonacci numbers starting with (0, 1). */
 lazy val fibs: LazyList[Int] = 0 #:: fibs.scanLeft(1) { (prev, curr) => prev + curr }
