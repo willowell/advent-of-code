d5027a2ae386277ce62f0b1481dec7d3d5d99470
William Howell <9555113+willowell@users.noreply.github.com>
2024-11-27 11:03:31 -0500

    Scaffold year 2024 day 01

---
+++ advent-of-code-scala/src/solutions/y2024/day01.scala
@@ -0,0 +1,21 @@
+/// Day 01: A Template Problem
+///
+///
+package solutions.y2024.day01
+
+import solutions.y2024.year
+import harness.*
+
+import core.parser.prelude.*
+
+import parsley.*
+import parsley.character.*
+import parsley.combinator.*
+import parsley.errors.combinator.*
+
+given day: Day = Day(1)
+object run extends AocDayRunner(part1, part2)
+
+def part1(input: String): Int = 0
+
+def part2(input: String): Int = 0
---
+++ advent-of-code-scala/src/solutions/y2024/day01.test.scala
@@ -0,0 +1,11 @@
+import solutions.y2024.day01.*
+
+class Y2024_Day01_Suite extends munit.FunSuite {
+  test("Example 1") {
+    assertEquals(part1(""), 0)
+  }
+
+  test("Example 2") {
+    assertEquals(part2(""), 0)
+  }
+}
