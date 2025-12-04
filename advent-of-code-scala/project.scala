f2586c98d93c764569d2297ea4cf62993e57c2ac
William Howell <9555113+willowell@users.noreply.github.com>
2025-12-04 10:24:47 -0500

    Update deps and formatting of project.scala

--- advent-of-code-scala/project.scala
+++ advent-of-code-scala/project.scala
@@ -10,55 +10,63 @@
 // SCALA VERSION AND PLATFORMS                                                                                        //
 //====================================================================================================================//
 
-//> using scala 3.3.3
+// Scala 3.3 LTS
+//> using scala 3.3
 
 //> using jvm 21
 
 //====================================================================================================================//
 // DEPENDENCIES                                                                                                       //
 //====================================================================================================================//
-/*
-  Dependencies Overview
-*/
+//
+// Dependencies Overview
+//
 
-/* Toolkits */
-//> using dep "org.typelevel::toolkit:0.1.27"
-//> using dep "org.typelevel::toolkit-test:0.1.27"
+// Toolkits
+//> using dep "org.typelevel::toolkit:0.1.29"
+//> using dep "org.typelevel::toolkit-test:0.1.29"
 
-/* Typelevel */
-//> using dep "org.typelevel::cats-core:2.12.0"
-//> using dep "org.typelevel::cats-free:2.12.0"
-//> using dep "org.typelevel::cats-mtl:1.5.0"
-//> using dep "org.typelevel::cats-parse:1.0.0"
-//> using dep "org.typelevel::kittens:3.4.0"
+// Typelevel
+//> using dep "org.typelevel::kittens:3.5.0"
+
 //> using dep "org.typelevel::spire:0.18.0"
 
-/* Circe */
-//> using dep "io.circe::circe-core:0.14.9"
-//> using dep "io.circe::circe-generic:0.14.9"
-//> using dep "io.circe::circe-parser:0.14.9"
+// Circe
+//> using dep "io.circe::circe-core:0.14.15"
+//> using dep "io.circe::circe-generic:0.14.15"
+//> using dep "io.circe::circe-parser:0.14.15"
 
-/* Monocle */
+// PureConfig
+//> using dep "com.github.pureconfig::pureconfig-core:0.17.9"
+//> using dep "com.github.pureconfig::pureconfig-cats-effect:0.17.9"
+
+// Monocle
 //> using dep "dev.optics::monocle-core:3.3.0"
 
-/* Scodec */
-//> using dep "org.scodec::scodec-bits:1.2.1"
+// Scodec
+//> using dep "org.scodec::scodec-bits:1.2.4"
 
-/* Parsley */
-//> using dep "com.github.j-mie6::parsley:4.5.2"
+// Parsley
+//> using dep "com.github.j-mie6::parsley:4.6.1"
 
-/* lihaoyi */
-//> using dep "com.lihaoyi::fansi:0.5.0"
-//> using dep "com.lihaoyi::os-lib:0.10.5"
+// lihaoyi
+//> using dep "com.lihaoyi::fansi:0.5.1"
+//> using dep "com.lihaoyi::os-lib:0.11.5"
 //> using dep "com.lihaoyi::requests:0.9.0"
+//> using dep "com.lihaoyi::sourcecode:0.4.4"
 
-/* sttp */
-//> using dep "com.softwaremill.sttp.client4::core:4.0.0-M5"
+// sttp
+//> using dep "com.softwaremill.sttp.client4::core:4.0.12"
 
-/* Math Stuff */
-//> using dep "ai.dragonfly::slash:0.3.1"
-//> using dep "com.manyangled::coulomb-core:0.8.0"
-//> using dep "com.manyangled::coulomb-units:0.8.0"
+// Math Stuff
+//> using dep "ai.dragonfly::slash:0.4.1"
+
+//> using dep "com.manyangled::coulomb-core:0.9.1"
+//> using dep "com.manyangled::coulomb-units:0.9.1"
+
+// Scrimage
+//> using dep "com.sksamuel.scrimage:scrimage-core:4.3.5"
+//> using dep "com.sksamuel.scrimage:scrimage-scala_2.13:4.3.5"
 
 //====================================================================================================================//
 // RESOURCES                                                                                                          //
@@ -69,9 +77,9 @@
 //====================================================================================================================//
 // SCALA COMPILER OPTIONS                                                                                             //
 //====================================================================================================================//
-/*
-  Scala Compiler Options Overview
-*/
+//
+// Scala Compiler Options Overview
+//
 
 //> using option -deprecation
 //> using option -explain
@@ -89,6 +97,6 @@
 //====================================================================================================================//
 // JAVA VIRTUAL MACHINE OPTIONS                                                                                       //
 //====================================================================================================================//
-/*
-  Java Virtual Machine Options Overview
-*/
+//
+// Java Virtual Machine Options Overview
+//
