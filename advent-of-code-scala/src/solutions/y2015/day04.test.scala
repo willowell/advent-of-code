39e679b25920d234750df7ee29e9d6327d31852c
William Howell <9555113+willowell@users.noreply.github.com>
2025-12-24 03:15:52 -0500

    Fix Part 2 test case for 2015 day 4

--- advent-of-code-scala/src/solutions/y2015/day04.test.scala
+++ advent-of-code-scala/src/solutions/y2015/day04.test.scala
@@ -8,6 +8,6 @@ class Y2015_Day04_Suite extends munit.FunSuite {
 
   test("Example 2") {
     assertEquals(part2("abcdef"), 6742839)
-    assertEquals(part2("pqrstuv"), 5714438)
+    assertEquals(part2("pqrstuv"), 25729123)
   }
 }
